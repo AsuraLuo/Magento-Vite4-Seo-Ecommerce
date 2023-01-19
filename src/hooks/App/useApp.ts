1d89b0b9c52e488882b6a2ad14e14639932c7119
caesar <caesar.luo@silksoftware.com>
2023-01-19 10:15:31 +0800

    chore: contentful rich text

---
+++ src/hooks/App/useApp.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import { createClient } from "contentful";
+
+export const useApp = () => {
+  const [content, setContent] = useState<any>(null);
+
+  const client = createClient({
+    space: import.meta.env.REACT_CONTENTFUL_SPACE_ID,
+    environment: import.meta.env.REACT_CONTENTFUL_ENVIRONMENT,
+    accessToken: import.meta.env.REACT_CONTENTFUL_ACCESS_TOKEN,
+  });
+
+  useEffect(() => {
+    client
+      .getEntry("52QiaxHdNPCne5qt9Q0IC5")
+      .then((entry: any) => {
+        const rawRichTextField = entry.fields.message;
+        setContent(rawRichTextField);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  return {
+    content,
+  };
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig, loadEnv, ConfigEnv } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }: ConfigEnv) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), "REACT_") };
+
+  return defineConfig({
+    envPrefix: "REACT_",
+    plugins: [react()],
+    server: {
+      host: "localhost",
+      port: 3000,
+      strictPort: true,
+      cors: true,
+      hmr: true,
+    },
+  });
+};
